7ce6a41d7edc1bfa5ec51d731ce1a27d00c5dcd0
BNargiz <nargiz493@gmail.com>
2022-06-16 14:50:32 +0200

    socket added

--- index.js
+++ index.js
@@ -5,9 +5,53 @@ const authMiddleWare = require("./auth/middleware");
 const authRouter = require("./routers/auth");
 const profileRouter = require("./routers/profileRouter");
 const { PORT } = require("./config/constants");
-
 // Create an express app
 const app = express();
+app.use(corsMiddleWare());
+
+//websocket
+const socketio = require("socket.io");
+const http = require("http");
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("we have connection");
+
+  socket.on("join", ({ userId, room }, callback) => {
+    console.log("joined conv", userId, room);
+
+    // socket.emit("message", {
+    //   user: "admin",
+    //   text: `${user.name} ,welcome to the room ${room}`,
+    // });
+    // socket.broadcast.to(room).emit("message", {
+    //   user: "admin",
+    //   text: `${user.name} ,has joined!`,
+    // });
+
+    socket.join(room);
+    // callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    io.to(message.room).emit("message", {
+      user: message.userId,
+      body: message.body,
+      room: message.room,
+      username: message.username,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user had left");
+  });
+});
 
 /**
  * Middlewares
@@ -20,7 +64,6 @@ const app = express();
 // CORS middleware:  * Since our api is hosted on a different domain than our client
 // we are are doing "Cross Origin Resource Sharing" (cors)
 // Cross origin resource sharing is disabled by express by default
-app.use(corsMiddleWare());
 
 // express.json():be able to read request bodies of JSON requests a.k.a. body-parser
 const bodyParserMiddleWare = express.json();
@@ -52,6 +95,6 @@ app.post("/authorized_post_request", authMiddleWare, (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
